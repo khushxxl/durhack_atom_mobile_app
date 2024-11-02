78e770b4b9e6893492d1e749d1948917984b2bb1
Khushaal Choithramani <khushaal.choithramani@gmail.com>
2024-11-02 18:11:14 +0000

    initial file

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
+import CustomText from "@/ui/CustomText";
 import { Link } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Page() {
   return <View className="flex flex-1"></View>;
